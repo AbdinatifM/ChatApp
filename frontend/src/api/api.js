c8c47cd64e4027b0f08de3de185988bd63c2c5a1
AbdinatifM <aj.notebook0@gmail.com>
2026-06-25 18:53:42 -0500

    testine env issues.

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+console.log("BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
+
 export const api = axios.create({
-    baseURL: import.meta.env.BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
 });
