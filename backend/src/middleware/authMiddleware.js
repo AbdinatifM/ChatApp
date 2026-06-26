0b6d518be9a8cd37be5feed0a98788529fa8aede
AbdinatifM <aj.notebook0@gmail.com>
2026-06-25 19:15:36 -0500

    debugging

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -6,17 +6,20 @@ const authMiddleware = async(req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
+            console.log("no token provided");
             return res.status(401).json({error: "no token provided"});
         }
 
         const decoded = jwt.verify(token,  process.env.JWT_SECRET);
         if (!decoded) {
+            console.log( "invalid token");
             return res.status(401).json({error: "invalid token"});
         }
 
         const user = await User.findById(decoded.userId);
 
         if (!user) {
+            console.log("User not found");
             return res.status(401).json({error: "User not found"});
         }
 
